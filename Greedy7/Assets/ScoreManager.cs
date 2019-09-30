50ba48515175c26db9d17b8e17dd1c3cd9a8aaf8
Kishankumar Naik <kishankn@usc.edu>
2019-09-30 14:54:17 -0700

    Initial game draft
    Working 3X3 board game configuration

---
+++ Greedy7/Assets/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text[] scoreTexts;
+    public static int scoreValA = 0, scoreValB = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Intializes scores of Player A and Player B's to 0
+        scoreTexts[0].text = "A: " + scoreValA;
+        scoreTexts[1].text = "B: " + scoreValB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updates scores of Player A and Player B's to 
+        // their respective scoreVal
+        scoreTexts[0].text = "A: " + scoreValA;
+        scoreTexts[1].text = "B: " + scoreValB;
+    }
+}
---
+++ Greedy7/Assets/Tiles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+    public int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // OnClick function for the tile 
+    public void clickFunction()
+    {
+        // Sends index of the tile to the TileManager Script
+        TileManager.updateScore(index);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
