7ad687046f95795ebc6efd7ca2f957ffb6202b09
Kishankumar Naik <kishankn@usc.edu>
2019-10-25 19:49:05 -0700

    Included previous tile array PowerUpTilesDto

--- Tutorial Prototype with Color/Assets/Scripts/PowerUps/PowerUpTilesDto.cs
+++ Tutorial Prototype with Color/Assets/Scripts/PowerUps/PowerUpTilesDto.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class PowerUpTilesDto
 {
     private Tile[] tileArray;
+    private Tile[] previousTileArray;
     private int tileIdToBeBlocked = -1;
+    private int tileIdToBeExploded = -1;
     private bool reverse = false;
 
-    public PowerUpTilesDto(Tile[] tileArray, int tileIdToBeBlocked, bool reverse)
+    public PowerUpTilesDto(Tile[] tileArray, Tile[] previousTileArray, int tileIdToBeBlocked, int tileIdToBeExploded, bool reverse)
     {
         this.tileArray = tileArray;
+        this.previousTileArray = tileArray;
         this.tileIdToBeBlocked = tileIdToBeBlocked;
+        this.tileIdToBeExploded = tileIdToBeExploded;
         this.reverse = reverse;
     }
 
     public Tile[] TileArray { get; set; }
 
+    public Tile[] PreviousTileArray { get; set; }
+
     public int TileIdToBeBlocked { get; set; }
+
+    public int TileIdToBeExploded { get; set; }
 
     public bool Reverse { get; set; }
 
