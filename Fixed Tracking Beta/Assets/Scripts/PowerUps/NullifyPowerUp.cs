873653cfd9e8d1b71f2568036b3d16f098c5b17d
Narongkrit (Tony) Khieopraphatsorn <31440970+narongkrit-tony@users.noreply.github.com>
2019-11-11 14:58:02 -0800

    Fixed Tracking Beta

---
+++ Fixed Tracking Beta/Assets/Scripts/PowerUps/NullifyPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NullifyPowerUp : PowerUpBase
+{
+    public string GetType()
+    {
+        return "nullify";
+    }
+
+    public PowerUpTilesDto Use(PowerUpTilesDto powerUpTilesDto)
+    {
+        return powerUpTilesDto;
+    }
+}
