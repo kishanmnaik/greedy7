94bb84c8966112d828c3d88b57ee817816ae7219
Keshav D Karanth <keshavkd.96@gmail.com>
2019-11-11 01:13:49 -0800

    Tracking with File Handling from Storage

---
+++ Tracking Beta/Assets/Scripts/PowerUps/BlockPowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPowerUp : PowerUpBase
+{
+    public string GetType()
+    {
+        return "shield";
+    }
+
+    public PowerUpTilesDto Use(PowerUpTilesDto powerUpTilesDto)
+    {
+        powerUpTilesDto.TileArray[powerUpTilesDto.TileIdToBeBlocked].setBlock();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log("ID: " + i + " : " + powerUpTilesDto.TileArray[i].getIsBlocked());
+        }
+
+        return powerUpTilesDto;
+    }
+}
