7f5f1d170b0500fb5a3db0a70f4f08872c3f0e5e
Narongkrit (Tony) Khieopraphatsorn <31440970+narongkrit-tony@users.noreply.github.com>
2019-11-16 23:55:54 -0800

    implementing instant buy

---
+++ Making instant buy/Assets/Scripts/PowerUps/PowerUpTilesDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpTilesDto
+{
+    public Tile[] TileArray { get; set; }
+
+    public Tile[] PreviousTileArray { get; set; }
+
+    public int TileIdToBeBlocked { get; set; }
+
+    public int TileIdToBeExploded { get; set; }
+
+    public int ScoreA { get; set; }
+
+    public int ScoreB { get; set; }
+
+    public int PreviousScoreA { get; set; }
+
+    public int PreviousScoreB { get; set; }
+
+    public bool Reverse { get; set; }
+}
---
+++ Making instant buy/Assets/Scripts/TrackingAnalytics.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+
+public class TrackingAnalytics : MonoBehaviour
+{
+    /* public int totalTurns = 0;
+    public float totalTime = 0.0f;
+    public string powerUpList = ""; */
+
+    private string path = "Assets/Tracking/t_beta_111119.csv";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // int totalTurns, float totalTime, string powerUpList, string usedPowerUpList
+    public void updateFile(int totalTurns, int totalTime, string powerUpList, string usedPowerUpList)
+    {
+        string line = "";
+        int gameCount = 1; 
+        try
+        {
+            StreamReader sr = new StreamReader(path);
+            line = sr.ReadLine();
+            while(true)
+            {
+                line = sr.ReadLine();
+                if (line == null)
+                    break;
+                gameCount++;
+                Debug.Log(line);
+            }
+
+            sr.Close();
+        } catch (Exception e)
+        {
+            Debug.Log("READ: File Does Not Exist.");
+        }
+
+        try
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.Write((gameCount+1) + "," + totalTurns + "," + totalTime + "," + powerUpList + "," + usedPowerUpList + "\n");
+            sw.Close();
+        } catch (Exception e)
+        {
+            Debug.Log("WRITE: File Does Not Exist.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
